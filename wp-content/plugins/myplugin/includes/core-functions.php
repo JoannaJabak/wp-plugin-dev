c525c0c5de46035a958eb8cb39624b5fdabbc58a
JoannaJabak <77950959+JoannaJabak@users.noreply.github.com>
2021-02-03 17:22:16 -0500

    completed "customize admin area" functionality

--- wp-content/plugins/myplugin/includes/core-functions.php
+++ wp-content/plugins/myplugin/includes/core-functions.php
@@ -82,7 +82,7 @@ function myplugin_custom_login_styles() {
             'myplugin',
             // 	string           $src = '', 
             plugin_dir_url( dirname( __FILE__ ) ) . 'public/css/myplugin-login.css', 
-            // 	array            $deps = array(), 
+            // 	array of dependencies  $deps = array(), 
             array(), 
             // 	string|bool|null $ver = false, 
             null, 
@@ -95,7 +95,7 @@ function myplugin_custom_login_styles() {
             'myplugin', 
             // 	string           $src = '', 
             plugin_dir_url( dirname( __FILE__ ) ) . 'public/js/myplugin-login.js', 
-            // 	array            $deps = array(), 
+            // 	array of dependencies  $deps = array(), 
             array(), 
             // 	string|bool|null $ver = false, 
             null, 
@@ -126,3 +126,81 @@ function myplugin_custom_login_message($message) {
 }
 // 4. Add a filter to change the message to display above the login form
 add_filter('login_message', 'myplugin_custom_login_message');
+
+
+//custom admin area footer
+function myplugin_custom_admin_footer($message) {
+    
+    //1. Use the get_option() function to get the plugin options 
+    $options = get_option('myplugin_options', myplugin_options_default());
+
+    // 2. Check if the custom footer is set, and is not empty. If so, sanitize the footer using sanitize_text_field()
+    if (isset($options['custom_footer']) && !empty($options['custom_footer'])) {
+        
+        $message = sanitize_text_field($options['custom_footer']);
+
+    }
+    // 3. Return the footer message
+    return $message;
+
+}
+// 4. Add a filter to change admin area footer text
+add_filter( 'admin_footer_text', 'myplugin_custom_admin_footer' );
+
+//custom admin area toolbar
+//removes the "New Post" and "Comments" links in the top toolbar
+function myplugin_custom_admin_toolbar() {
+    // 1. Set the $toolbar to false as the default value (since the default checkbox is unchecked)
+    $toolbar = false;
+
+    // 2. Use the get_option() function to get the plugin options 
+    $options = get_option('myplugin_options', myplugin_options_default());
+
+    // 3. Check if the customized toolbar option is enabled. 
+    if (isset($options['custom_toolbar']) && !empty($options['custom_toolbar'])) {
+        
+        // Use (bool) to make sure that the variable is converted into a boolean
+        $toolbar = (bool) $options['custom_toolbar'];
+
+    }
+
+    if ($toolbar) {
+        //define global $wp_admin_bar to access the global WordPress variable inside the scope of this function
+        global $wp_admin_bar;
+
+        //Remove 'comments' and 'new-content' from the admin bar
+        $wp_admin_bar->remove_menu('comments');
+        $wp_admin_bar->remove_menu('new-content');
+
+    }
+}
+// 4. Add an action before the admin bar rendering
+add_action('wp_before_admin_bar_render', 'myplugin_custom_admin_toolbar', 99);
+
+//custom color scheme
+function myplugin_custom_admin_scheme ($user_id) {
+    // 1. Set the $scheme to 'default' as the default value
+    $scheme = 'default';
+    
+    // 2. Use the get_option() function to get the plugin options 
+    $options = get_option ('myplugin_options', myplugin_options_default());
+
+    // 2. Check if the custom scheme is set, and is not empty. If so, sanitize the footer using sanitize_text_field()
+    if (isset ($options['custom_scheme']) && !empty($options['custom_scheme'])) {
+
+        $scheme = sanitize_text_field($options['custom_scheme']);
+
+    }
+
+
+    // 3. update the user's color scheme option using wp_update_user
+    $args = [
+        'ID' => $user_id,
+        'admin_color' => $scheme,
+    ];
+
+    wp_update_user( $args );
+
+}
+
+add_action('user_register', 'myplugin_custom_admin_scheme');
